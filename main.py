85f48348c616bf1ac1792838a22b377b4ac211f0
Kruthvik Maruboina <70449973+kruthvik@users.noreply.github.com>
2025-10-25 21:02:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from googleapiclient.http import MediaIoBaseUpload
 from google.oauth2 import service_account
 import io
 from tqdm import tqdm
+from findAgency import getAgency
+from datetime import datetime as dt
 
 scope = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,8 +17,7 @@ scope = [
 creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
 client = gs.authorize(creds)
 
-
-notices = client.open_by_url("GSHEET URL").sheet1
+notices = client.open_by_url("INSERT API ").sheet1
 
 
 def getNumCol(): 
@@ -26,12 +27,11 @@ def getNumCol():
 
 def fetch_taken_terms():
     numCol = getNumCol()
-    print(numCol)
     return [notices.cell(i, 1).value for i in range(2, numCol + 1)]
 
 
 # uses the np and terms vars to fetch data from federalregister.gov
-def scrape(np, terms):
+def scrape(np, terms, numCols):
     terms = terms.split(',')
     terms = [i[1:] if i[0] == " " else i for i in terms]
     
@@ -51,20 +51,38 @@ def scrape(np, terms):
 
         res = [i for i in res if any(j in i["title"].lower() for j in searchTerms)]
 
-    
+        nc = 0
         takenTerms = fetch_taken_terms()
         for i in res:
+            if nc == numCols:
+                return
+
             if i["title"] not in takenTerms:
                 docNum = i['document_number']
-                print(i["document_number"])
-
                 commentDateUrl = f"https://www.federalregister.gov/api/v1/documents/{docNum}.json?fields[]=comments_close_on"
                 commentDate = req.get(commentDateUrl).json()
-                finalCommentDate = commentDate["comments_close_on"]
-                    
+                finalCommentDate = commentDate["comments_close_on"] 
 
-                agencyNames = [m['name'] for m in i['agencies']]
-                newrow = [i["title"], ', '.join(agencyNames), i['publication_date'], finalCommentDate, i['abstract'], i['html_url'], ""]
+                print("Adding notice:", i["title"])
+                
+                publicationDate = dt.strptime(i['publication_date'], "%Y-%m-%d").strftime("%m/%d/%Y")
+                
+                if finalCommentDate:
+                    finalCommentDate = dt.strptime(finalCommentDate, "%Y-%m-%d").strftime("%m/%d/%Y")
+
+                newrow = [i["title"], getAgency(docNum).title(), publicationDate, f"{publicationDate} - {finalCommentDate}", i['abstract'], i['html_url'], ""]
                 notices.append_row(newrow)
-                    
-scrape(500,"artificial intelligence")
+                
+                nc += 1
+
+              
+
+if __name__ == "__main__":
+    numPages = input("Enter amount of pages to search: ")
+    terms = input("Enter terms to search (separate by commas) (type nothing for all): ")
+    
+    if terms.strip() == "":
+        terms = "artificial intelligence, foundation models, automated decision-making, machine learning, ai, ai systems, ai technologies, ai tools, ai applications, ai software, ai algorithms, ai regulation, ai policy"
+    
+    numCols = input("Enter number of notices to add: ")
+    scrape(numPages, terms, int(numCols))
